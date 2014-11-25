3dbf89aec72a9ffa16b66839e7bba7f0df886f69
Federico Aloi <federico.aloi@gmail.com>
2014-11-25 03:57:32 -0300

    Desharcodeados los datos del HTML

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,22 +1,75 @@
 'use strict';
 
+var Actividad = function(nombre, asiste) {
+  this.nombre = nombre;
+  this.asiste = asiste;
+}
+
+Actividad.prototype.cambiarAsistencia = function() {
+  this.asiste = !this.asiste;
+};
+
+Actividad.prototype.icon = function() {
+  return this.checkearAsistencia("fa-check", "fa-remove", "fa-minus");
+};
+
+Actividad.prototype.color = function() {
+  return this.checkearAsistencia("btn-success", "btn-danger", "btn-default");
+};
+
+Actividad.prototype.checkearAsistencia = function(valorTrue, valorFalse, valorNull) {
+  switch (this.asiste) {
+    case true:
+      return valorTrue;
+
+    case false:
+      return valorFalse;
+
+    default:
+      return valorNull;
+  };
+}
+
 angular.module('wisitTrackerApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
+  .controller('MainCtrl', function ($scope) {
+    $scope.actividades = _.map([
+      {
+        nombre: "Experiencias de la construcción de una carrera de programación",
+        asiste: null
+      },
+      {
+        nombre: "Pleasy Web: Inclusion Digital",
+        asiste: true
+      },
+      {
+        nombre: "Computadora Industrial Abierta Argentina (CIAA)",
+        asiste: false
+      },
+      {
+        nombre: "Construyendo una infraestructura de Big Data rentable y escalable",
+        asiste: null
+      },
+      {
+        nombre: "Escalar sin antecedentes - Una Aventura de Microservicios",
+        asiste: null
+      },
+      {
+        nombre: "Utilización de NoSQL para resolución de problemas al trabajar con cantidades masivas de datos",
+        asiste: null
+      },
+      {
+        nombre: "En camino hacia Java 8",
+        asiste: null
+      },
+      {
+        nombre: "Implementando Puppet en una agencia estatal en Argentina",
+        asiste: null
+      },
+      {
+        nombre: "Un sistema para 500 millones de inversiones",
+        asiste: null
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+    ], function(actividadJson) {
+      return new Actividad(actividadJson.nombre, actividadJson.asiste);
+    });
   });
