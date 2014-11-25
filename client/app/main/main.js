a1882c6d863d28dfa0c3bdcda3eb72c63816e5a8
Federico Aloi <federico.aloi@gmail.com>
2014-11-25 01:46:05 -0300

    Creado proyecto con generator angular-fullstack de Yeoman

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('wisitTrackerApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
